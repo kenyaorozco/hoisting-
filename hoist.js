45041ad44a6644d7a84fcc6beaf1c0b4aba0e053
Kenya Orozco <109471756+kenyaorozco@users.noreply.github.com>
2022-11-28 17:49:41 -0800

    Add files via upload

---
+++ hoist.js
@@ -0,0 +1,138 @@
+// console.log(hello);                                   
+// var hello = 'world'; 
+
+
+var hello ;
+console.log(hello)
+hello = 'world';
+
+// var needle = 'haystack';
+// test();
+// function test(){
+//     var needle = 'magnet';
+//     console.log(needle);
+// }
+
+var needle;
+function test(){
+    var needle;
+    var needle = 'magnet';
+    console.log(needle);
+}
+test();
+needle = 'haystack'
+
+// var brendan = 'super cool';
+// function print(){
+//     brendan = 'only okay';
+//     console.log(brendan);
+// }
+// console.log(brendan);
+
+var brendan;
+var brendan = 'super cool';
+function print(){
+    var brendan;
+    console.log(brendan);
+    brendan = 'only okay';
+}
+console.log(brendan);
+
+
+// var food = 'chicken';
+// console.log(food);
+// eat();
+// function eat(){
+//     food = 'half-chicken';
+//     console.log(food);
+//     var food = 'gone';
+// }
+
+var food ;
+console.log(food);
+var food = 'chicken';
+function eat(){
+    var food;
+    console.log(food);
+    var food = 'gone';
+    food = 'half-chicken';
+}
+eat();
+
+
+// mean();
+// console.log(food);
+// var mean = function() {
+//     food = "chicken";
+//     console.log(food);
+//     var food = "fish";
+//     console.log(food);
+// }
+// console.log(food);
+
+var mean = function;
+mean();
+var food;
+console.log(food);
+var mean = function() {
+    var food;
+    var food = "fish";
+    food = "chicken";
+    console.log(food);
+    console.log(food);
+}
+console.log(food);
+
+// console.log(genre);
+// var genre = "disco";
+// rewind();
+// function rewind() {
+//     genre = "rock";
+//     console.log(genre);
+//     var genre = "r&b";
+//     console.log(genre);
+// }
+// console.log(genre);
+
+var genre 
+console.log(genre);
+var genre = "disco";
+// calling function
+rewind();
+function rewind() {
+    var genre;
+    genre = "rock";
+    console.log(genre);
+    var genre;
+    genre = "r&b";
+    console.log(genre);
+}
+console.log(genre);
+
+
+// dojo = "san jose";
+// console.log(dojo);
+// learn();
+// function learn() {
+//     dojo = "seattle";
+//     console.log(dojo);
+//     var dojo = "burbank";
+//     console.log(dojo);
+// }
+// console.log(dojo);
+
+var dojo 
+console.log(dojo);
+dojo = "san jose";
+learn();
+function learn() {
+    var dojo;
+    console.log(dojo);
+    dojo = "seattle";
+    dojo = "burbank";
+    console.log(dojo);
+}
+console.log(dojo);
+
+
+
